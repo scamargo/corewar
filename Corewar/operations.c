ee87387cee468f9d824df47dee44380be905c04d
Sergio Camargo <sergio.camargo813@gmail.com>
2018-03-05 18:40:57 -0800

    add purge function for cleaning processes

--- Corewar/operations.c
+++ Corewar/operations.c
@@ -6,16 +6,31 @@
 /*   By: tyang <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 11:50:17 by tyang             #+#    #+#             */
-/*   Updated: 2018/03/05 11:59:45 by tyang            ###   ########.fr       */
+/*   Updated: 2018/03/05 18:40:32 by scamargo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "vm.h"
+#include "libft.h"
 
-bool	purge(t_list **all_process)
+void	delete_process(void *process, size_t size)
 {
-	while (*all_process)
+	// TODO: free registry array
+	free(process->registries);
+	free(process);
+}
+
+bool	purge(t_doubly *process_node)
+{
+	t_process *current_process;
+
+	while (process_node)
 	{
-		if (*all_process->alive == 0)
-			
+		current_process = (t_process*)process_node->content; 
+		if (current_process->alive)
+			current_process->alive = 0;
+		else
+			ft_doublydelone(&process_node, delete_process);
+		process_node = process_node->next;
+	}
 }
